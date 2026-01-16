d708a29bee483a654071fbbc4ab427ff91f8cdbf
kapilsarkar <k.sarkar81@gmail.com>
2026-01-16 09:21:01 +0530

    functionfileadded

--- JS REFRESHER/function.js
+++ JS REFRESHER/function.js
@@ -4,5 +4,45 @@ Store the result in a variable named `teaOrder`.
 */
 
 function makeTea(typeOfTea) {
-    return
+    return `Making ${typeOfTea}`;
+}
+
+let teaOrder = makeTea("lemon tea");;
+console.log(teaOrder);
+
+
+/* 
+2. Create a function named `orderTea` that takes one parameter, `teaType`. Inside this function, create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"`. 
+Call `confirmOrder` from within `orderTea` and return the result.
+*/
+
+function orderTea(teaType) {
+    function confirmOrder() {
+        return `Order confirmed for ${teaType}`;
+    }
+
+    return confirmOrder();
+}
+
+let orderConfirmation = orderTea("chai");
+console.log(orderConfirmation);
+
+/* 
+3. Write an arrow function named `calculateTotal` that takes two parameters: `price` and `quantity`. The function should return the total cost by multiplying the `price` and `quantity`. 
+Store the result in a variable named `totalCost`.
+*/
+
+const calculateTotal = (price, quantity) => {
+    return totalCost = price * quantity;
+}
+
+console.log(calculateTotal(20,70));
+
+/* 
+4. Write a function named `processTeaOrder` that takes another function, `makeTea`, as a parameter and calls it with the argument `"earl grey"`. 
+Return the result of calling `makeTea`.
+*/
+
+function processTeaOrder(makeTea){
+
 }
