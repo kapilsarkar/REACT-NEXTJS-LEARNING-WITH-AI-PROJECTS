40c08170e4e9bbab7e6885fe3c8d579ccdb2e8ff
kapilsarkar <k.sarkar81@gmail.com>
2026-01-18 08:05:43 +0530

    functionfileadded

--- JS REFRESHER/function.js
+++ JS REFRESHER/function.js
@@ -36,13 +36,32 @@ const calculateTotal = (price, quantity) => {
     return totalCost = price * quantity;
 }
 
-console.log(calculateTotal(20,70));
+console.log(calculateTotal(20, 70));
 
 /* 
 4. Write a function named `processTeaOrder` that takes another function, `makeTea`, as a parameter and calls it with the argument `"earl grey"`. 
 Return the result of calling `makeTea`.
 */
-
-function processTeaOrder(makeTea){
-
+function makeTea(typeOfTea) {
+    return `Make tea : ${typeOfTea}`;
 }
+
+function processTeaOrder(teaFunction) {
+    return teaFunction("earl grey");
+}
+
+let order = processTeaOrder(makeTea);
+console.log(order);
+
+/* 
+5. Write a function named `createTeaMaker` that returns another function. The returned function should take one parameter, `teaType`, and return a message like `"Making green tea"`. 
+Store the returned function in a variable named `teaMaker` and call it with `"green tea"`.
+*/
+function createTeaMaker() {
+    return function (teaType) {
+        return `Making ${teaType}`;
+    }
+}
+
+let teaMaker = createTeaMaker();
+console.log(teaMaker("green tea"));
