a42efd4713aa5aefd43a88ef85f758a8490e1ae9
kapilsarkar <k.sarkar81@gmail.com>
2026-01-25 09:23:38 +0530

    moduleimportexprtadded

---
+++ JS REFRESHER/PART-3/appC.js
@@ -0,0 +1,5 @@
+const mathOperation = require("./mathC.js")
+
+console.log(mathOperation.add(2,10));
+console.log(mathOperation.subtract(10,5));
+console.log(mathOperation.multiply(4,5));
---
+++ JS REFRESHER/PART-3/appM.js
@@ -0,0 +1,12 @@
+//Module Import File
+
+//default import
+import multiply from "./mathM.js";
+
+//named import
+import {add,subtract} from "./mathM.js";
+
+console.log(multiply(2,3));
+console.log(add(2,3));
+console.log(subtract(3,12))
+
