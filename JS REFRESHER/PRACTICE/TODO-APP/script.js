e976de5e318fe45c185b6aa85cb66403681f1b76
kapilsarkar <k.sarkar81@gmail.com>
2026-02-05 07:16:58 +0530

    addtaskadded

---
+++ JS REFRESHER/PRACTICE/TODO-APP/script.js
@@ -0,0 +1,22 @@
+//alert("hello");
+
+const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+const tasks = [];
+addTaskButton.addEventListener("click", function () {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        completed: false,
+
+    }
+
+    tasks.push(newTask);
+    todoInput.value = ""; //clear input
+    console.log(tasks);
+})
