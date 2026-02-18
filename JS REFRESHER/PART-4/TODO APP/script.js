f3889f5b65dbfa3ba7a4c18dcea15ef5da874779
kapilsarkar <k.sarkar81@gmail.com>
2026-02-18 07:12:04 +0530

    learningfilesadded

--- JS REFRESHER/PART-4/TODO APP/script.js
+++ JS REFRESHER/PART-4/TODO APP/script.js
@@ -1,56 +1,37 @@
-//alert("hello");
-
 document.addEventListener("DOMContentLoaded", () => {
+    //alert("hello");
+
     const todoInput = document.getElementById("todo-input");
     const addTaskButton = document.getElementById("add-task-btn");
     const todoList = document.getElementById("todo-list");
 
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    tasks.forEach(task => renderTask(task));
+    tasks.forEach(task => renderTask(task))
 
-    addTaskButton.addEventListener("click", () => {
+    addTaskButton.addEventListener("click", function () {
         const taskText = todoInput.value.trim();
-
-        if (taskText === "") return;
+        if (taskText === "") return
 
         const newTask = {
             id: Date.now(),
             text: taskText,
             completed: false,
+
         }
 
         tasks.push(newTask);
         saveTasks();
-        renderTask(newTask)
         todoInput.value = ""; //clear input
         console.log(tasks);
     })
+
     function renderTask(task) {
-        const li = document.createElement("li");
-        li.setAttribute("data-id", task.id);
-        if (task.completed) li.classList.add("completed");
-        li.innerHTML = `<span>${task.text}</span>
-        <button class="delete-btn">Delete</button>`;
-        li.addEventListener('click', (e) => {
-            if (e.target.tagName === 'BUTTON') return;
-            task.completed = !task.completed;
-            li.classList.toggle("completed");
-            saveTasks();
-        })
-        li.querySelector(".delete-btn").addEventListener("click", (e) => {
-            e.stopPropagation(); //prevent toggle from firing
-            tasks = tasks.filter((t) => t.id !== task.id);
-            li.remove();
-            saveTasks();
-        })
-        todoList.appendChild(li);
+        console.log(task);
     }
 
     function saveTasks() {
-        localStorage.setItem('tasks', JSON.stringify(tasks))
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     }
+
 })
-
-
-
