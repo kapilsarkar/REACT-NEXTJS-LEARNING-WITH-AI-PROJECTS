d89fe965796e92b2da43bb74bf88fd43db25d47d
kapilsarkar <k.sarkar81@gmail.com>
2026-03-29 08:33:18 +0530

    appadded

--- REACT/03-COUNTER PROJECT/src/App.jsx
+++ REACT/03-COUNTER PROJECT/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
-
+  const [inputValue, setInputValue] = useState("");
+  const handleDecrement = () => {
+    setCount((prev) => (prev > 0 ? prev - 1 : prev));
+  };
   return (
     <div className="bg-slate-900 text-white min-h-screen flex justify-center items-center">
       <div className="bg-slate-800 p-6 rounded-xl shadow-lg text-center space-y-4">
@@ -14,29 +17,43 @@ export default function App() {
         <div className="flex gap-3 justify-center">
           <button
             onClick={() => {
-              setCount(count + 1);
+              setCount((prev) => prev + 1);
             }}
             className="p-3 bg-green-600 hover:bg-green-700 rounded-lg"
           >
             Increase
           </button>
-          <button  className="p-3 bg-red-600 hover:bg-red-700 rounded-lg">
+          <button
+            onClick={() => handleDecrement()}
+            className="p-3 bg-red-600 hover:bg-red-700 rounded-lg"
+          >
             Decrease
           </button>
-          <button className="p-3 bg-gray-600 hover:bg-gray-700 rounded-lg">
+          <button
+            onClick={() => setCount(0)}
+            className="p-3 bg-gray-600 hover:bg-gray-700 rounded-lg"
+          >
             Reset
           </button>
         </div>
 
         <div className="flex gap-3 justify-center items-center">
           <input
-            onChange={() => {}}
+            onChange={(e) => {
+              setInputValue(Number(e.target.value));
+            }}
             className="text-black w-20 border-2 border-white px-2 py-1 rounded"
-            type="text"
-            value="5"
+            type="number"
+            value={inputValue}
           />
-          <button className="p-3 bg-blue-600 hover:bg-blue-700 rounded-lg">
-            Set
+          <button
+            onClick={() => {
+              setCount(Number(inputValue));
+              setInputValue(0);
+            }}
+            className="p-3 bg-blue-600 hover:bg-blue-700 rounded-lg"
+          >
+            Set To {inputValue}
           </button>
         </div>
       </div>
