568ade87e720d336b29a665ffd27093cab54601d
kapilsarkar <k.sarkar81@gmail.com>
2026-04-24 08:19:11 +0530

    newproject

---
+++ REACT/04-QUEUE MANAGEMENT SYSTEM/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Queue Management Application</h1>
+        <p> Manage Your Customer Efficiently</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
