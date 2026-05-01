e85cbfb3fb3b77535dfc69cc9daf2e286b792cd0
kapilsarkar <k.sarkar81@gmail.com>
2026-05-01 08:48:20 +0530

    componenetsadded

--- REACT/04-QUEUE MANAGEMENT SYSTEM/src/App.jsx
+++ REACT/04-QUEUE MANAGEMENT SYSTEM/src/App.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+import QueueForm from "./components/QueueForm";
+import QueueDisplay from "./components/QueueDisplay";
+
 function App() {
+  const [queue, setQueue] = useState([]);
+
+  const addToQueue = (customer) => {
+    //add data to queue
+    setQueue([...queue, { ...customer, id: Date.now(), status: "waiting" }]);
+  };
+  const updateStatus = (id, newStatus) => {
+    //change data in queue
+    setQueue(
+      queue.map((customer) => {
+        customer.id === id ? { ...customer, status: newStatus } : customer;
+      }),
+    );
+  };
+  const removeFromQueue = (id) => {
+    //remove data from queue
+    setQueue(queue.filter((customer) => customer.id !== id));
+  };
   return (
     <div className="app">
       <header>
         <h1>Queue Management Application</h1>
         <p> Manage Your Customer Efficiently</p>
       </header>
+      <main>
+        <QueueForm onAdd={addToQueue} />
+        <QueueDisplay
+          queue={queue}
+          onUpdateStatus={updateStatus}
+          onRemove={removeFromQueue}
+        />
+      </main>
     </div>
   );
 }
