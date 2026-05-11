6b324cfbb2042a77ebc8bc9c1862cc1f1d81ba42
kapilsarkar <k.sarkar81@gmail.com>
2026-05-11 07:13:26 +0530

    formdataadded

--- REACT/PRACTICE/QUEUE-GAME/src/App.jsx
+++ REACT/PRACTICE/QUEUE-GAME/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
+import QueueForm from "./components/QueueForm";
 
 function App() {
-  const [queue,setQueue] = useState([]);
+  const [queue, setQueue] = useState([]);
 
+  const addToQueue = (customer) => {
+    setQueue([...queue, { ...customer, id: Date.now(), status: "waiting" }]);
+    
+  };
   return (
     <>
       <header>
         <h2 className="text-center">QUEUE-GAME</h2>
       </header>
+      ,
       <main>
-        
+        <QueueForm onAdd={addToQueue} />
       </main>
     </>
   );
--- REACT/PRACTICE/QUEUE-GAME/src/components/QueueForm.jsx
+++ REACT/PRACTICE/QUEUE-GAME/src/components/QueueForm.jsx
@@ -1,8 +1,51 @@
-const QueueForm = () => {
+import { useState } from "react";
+import { FaUserPlus } from "react-icons/fa";
+const QueueForm = ({ onAdd }) => {
+  const [name, setName] = useState("");
+  const [service, setService] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //validations
+    if (!name.trim() || !service.trim()) return;
+    console.log(name, service);
+    onAdd({ name, service });
+
+    setName("");
+    setService("");
+  };
+
   return (
     <>
       <div>
         <h2 className=" font-bold">Queue Form</h2>
+        <form onSubmit={handleSubmit}>
+          <h2>Add To Queue</h2>
+          <div>
+            <h3>Select Name</h3>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <h3>Select Details</h3>
+            <select
+              value={service}
+              onChange={(e) => setService(e.target.value)}
+            >
+              <option value="">Select Service</option>
+              <option value="consultation">consultation</option>
+              <option value="payment">payment</option>
+              <option value="support">support</option>
+            </select>
+          </div>
+          <button type="submit">
+            <FaUserPlus />
+            Submit
+          </button>
+        </form>
       </div>
     </>
   );
