cd4d84bc12f71c9baa9712023c238e1264ab558c
kapilsarkar <k.sarkar81@gmail.com>
2026-07-05 06:43:05 +0530

    somedataadded

---
+++ REACT/07-ZUSTAND/doctors.js
@@ -0,0 +1,33 @@
+export const doctors = [
+  {
+    id: 1,
+    name: "Dr. Amit Sharma",
+    specialization: "Cardiologist",
+    experience: 12,
+    consultationFee: 800,
+    rating: 4.8,
+    hospital: "Apollo Hospital",
+    location: "Kolkata",
+    image: "/doctors/doctor1.jpg",
+    availableSlots: [
+      "09:00 AM",
+      "10:00 AM",
+      "11:30 AM"
+    ]
+  },
+  {
+    id: 2,
+    name: "Dr. Priya Singh",
+    specialization: "Dermatologist",
+    experience: 8,
+    consultationFee: 600,
+    rating: 4.7,
+    hospital: "Fortis Hospital",
+    location: "Durgapur",
+    image: "/doctors/doctor2.jpg",
+    availableSlots: [
+      "02:00 PM",
+      "03:30 PM"
+    ]
+  }
+];
