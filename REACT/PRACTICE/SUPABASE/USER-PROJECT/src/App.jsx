2fbe7416fd59437cbb0af2c58e77428aa8859818
kapilsarkar <k.sarkar81@gmail.com>
2026-08-28 16:46:50 +0530

    supabaseconfigadded

---
+++ REACT/PRACTICE/SUPABASE/USER-PROJECT/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Task from "./components/Task";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>USER-SYSTEM</h2>
+        <Task/>
+      </div>
+    </>
+  );
+}
+
+export default App;
