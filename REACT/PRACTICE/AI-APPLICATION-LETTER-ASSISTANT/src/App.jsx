c16c4b94270c61299681f03807e6aebd13d88260
kapilsarkar <k.sarkar81@gmail.com>
2026-09-06 09:23:52 +0530

    smalchangesaddedinapp

--- REACT/PRACTICE/AI-APPLICATION-LETTER-ASSISTANT/src/App.jsx
+++ REACT/PRACTICE/AI-APPLICATION-LETTER-ASSISTANT/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>AI-Application-Letter-Assistant</h1>
+      <h1 className="text-4xl font-bold">AI Application & Letter Assistant</h1>
     </>
   );
 }
