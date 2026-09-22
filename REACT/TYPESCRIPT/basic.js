9cc7492cf414827cb7e276c296c0b815ca8be3df
kapilsarkar <k.sarkar81@gmail.com>
2026-09-22 07:25:29 +0530

    intialcommit

--- REACT/TYPESCRIPT/basic.js
+++
@@ -1,6 +0,0 @@
-function greet(name){
-    return "Hello, " + name + "!";
-}
-
-console.log(greet("Alice")); // Output: Hello, Alice!
-console.log(greet(123))
---
+++ REACT/TYPESCRIPT/src/index.ts
@@ -0,0 +1,6 @@
+function  greet(person:string) : string {
+    return `Hello ${person}, Welcome to Type Script Learning`;
+}
+
+const userName : string = 'Kapil Sarkar';
+console.log(greet(userName));
