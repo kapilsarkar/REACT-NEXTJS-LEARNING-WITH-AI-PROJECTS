4625efe65cca4a77e62538131dcbbe65a2c74017
kapilsarkar <k.sarkar81@gmail.com>
2026-09-26 17:22:56 +0530

    intialcommitonprimitive

---
+++ TYPESCRIPT/src/02_primitive.ts
@@ -0,0 +1,9 @@
+let userName : string = "Kapil Sarkar";
+let age : number = 34;
+let isCreator : boolean = true;
+
+const big : bigint = 2n ** 63n - 1n
+
+//Operator '+' cannot be applied to types 'bigint' and 'number'.
+//const mixed = big + age
+
